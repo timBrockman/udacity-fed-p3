458af955cdaa5b267c1532b4025aef630e0409ea
timBrockman <tim@timbrockman.com>
2015-10-24 00:44:40 -0400

    feat: add player class to app.js

--- js/app.js
+++ js/app.js
@@ -24,7 +24,12 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function(){
+    this.sprite = 'images/char-boy.png';
+};
+Player.prototype.update = function(){};
+Player.prototype.render = function(){};
+Player.prototype.handleInput = function(){};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
